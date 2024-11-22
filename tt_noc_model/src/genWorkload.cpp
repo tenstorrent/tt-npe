bbe8411d3bba567e6c60a35599ed18c572424380
Brett Grady <bgrady@tenstorrent.com>
2024-11-22 16:51:11 +0000

    improve random workload to account for limited source injection rate

--- tt_noc_model/src/genWorkload.cpp
+++ tt_noc_model/src/genWorkload.cpp
@@ -2,33 +2,39 @@
 
 #include <unordered_map>
 
+#include "grid.hpp"
 #include "util.hpp"
 
 tt_npe::nocWorkload genRandomizedWorkload(
     const tt_npe::nocModel &model, const std::unordered_map<std::string, float> &params) {
     tt_npe::nocWorkload wl;
 
-    const int NUM_TRANSFERS = tt_npe::getWithDefault(params, "num_transfers", 1.0f);
-    const size_t PACKET_SIZE = tt_npe::getWithDefault(params, "packet_size", 1.0f);
+    const int num_transfers = tt_npe::getWithDefault(params, "num_transfers", 1.0f);
+    const size_t packet_size = tt_npe::getWithDefault(params, "packet_size", 1.0f);
+    const size_t injection_rate = tt_npe::getWithDefault(params, "injection_rate", 1.0f);
+    const size_t num_packets = tt_npe::getWithDefault(params, "num_packets", 1.0f);
 
     // construct one big phas with a bunch of random transfers
     tt_npe::nocWorkloadPhase ph;
-    ph.transfers.reserve(NUM_TRANSFERS);
+    ph.transfers.reserve(num_transfers);
     size_t total_bytes_overall = 0;
-    for (int i = 0; i < NUM_TRANSFERS; i++) {
-        auto src = tt_npe::Coord{1, (rand() % model.getCols())};
-        auto dst = tt_npe::Coord{1, (rand() % model.getCols())};
+    CycleCount stagger = 0;
+    tt_npe::Grid2D<int> transfer_per_src_loc(model.getRows(),model.getCols());
+    for (int i = 0; i < num_transfers; i++) {
+        auto src = tt_npe::Coord{rand() % 2, (rand() % 2)};
+        auto dst = tt_npe::Coord{rand() % model.getRows(), (rand() % model.getCols())};
         CycleCount startup_latency = (src.row == dst.row) || (src.col == dst.col) ? 155 : 260;
-        startup_latency += rand() % 512;
-        auto num_packets = ((rand() % 8) + 2);
-        total_bytes_overall += PACKET_SIZE * num_packets;
+        startup_latency += transfer_per_src_loc(src.row,src.col) * ((packet_size*num_packets)/injection_rate);
+        transfer_per_src_loc(src.row,src.col)++;
+
+        total_bytes_overall += packet_size * num_packets;
 
         tt_npe::nocWorkloadTransfer tr{
-            .packet_size = PACKET_SIZE,
+            .packet_size = packet_size,
             .num_packets = num_packets,
             .src = src,
             .dst = dst,
-            .injection_rate = 28.1,
+            .injection_rate = injection_rate,
             .cycle_offset = startup_latency};
         ph.transfers.push_back(tr);
     }
