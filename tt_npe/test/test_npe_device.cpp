40dd18a48d059df4cbb1aeeeddd347fbbb0b67fb
Brett Grady <bgrady@tenstorrent.com>
2024-12-16 20:51:25 +0000

    add unit test cmake build

---
+++ tt_npe/test/test_npe_device.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "npeDeviceModel.hpp"
+
+//TEST(npeDeviceTest, CanConstructWormholeB0Device) {
+//    auto maybe_device_model = tt_npe::npeDeviceModel::makeDeviceModel("wormhole_b0");
+//    EXPECT_TRUE(maybe_device_model.has_value());
+//}
+//TEST(npeDeviceTest, CanReturnEmptyDevice) {
+//    auto maybe_device_model = tt_npe::npeDeviceModel::makeDeviceModel("worthold_a0");
+//    EXPECT_FALSE(maybe_device_model.has_value());
+//}
