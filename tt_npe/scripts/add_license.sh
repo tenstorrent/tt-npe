8e39ea6e618c00709f0c1a9b7035f6ad493079a5
Brett Grady <bgrady@tenstorrent.com>
2025-01-28 21:49:17 +0000

    add misc missing scripts

---
+++ tt_npe/scripts/add_license.sh
@@ -0,0 +1,12 @@
+read -r -d '' LICTEXT << 'END'
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+END
+
+for f in `find -name '*.*py'`; do
+	printf "$LICTEXT\n\n" > tmp
+	cat $f          >> tmp
+	echo $f
+	#diff tmp $f
+	mv tmp $f
+done
