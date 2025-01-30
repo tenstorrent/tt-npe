b29f36e1bbbdd1fccc091224d2fecca09265147f
Brett Grady <bgrady@tenstorrent.com>
2025-01-30 15:51:22 +0000

    add licenses to new files

--- tt_npe/scripts/add_license.sh
+++ tt_npe/scripts/add_license.sh
@@ -1,12 +1,12 @@
 read -r -d '' LICTEXT << 'END'
-# SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 END
 
-for f in `find -name '*.*py'`; do
+for f in $@; do
 	printf "$LICTEXT\n\n" > tmp
 	cat $f          >> tmp
 	echo $f
-	#diff tmp $f
+	diff tmp $f
 	mv tmp $f
 done
