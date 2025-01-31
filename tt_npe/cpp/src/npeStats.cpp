0e484ffa5ec0f2aab8ea87f54b5b5ef0ae868d33
Brett Grady <bgrady@tenstorrent.com>
2025-01-31 15:06:35 +0000

    add more descriptive stats to npe_stats json output

--- tt_npe/cpp/src/npeStats.cpp
+++ tt_npe/cpp/src/npeStats.cpp
@@ -7,6 +7,7 @@
 
 #include "fmt/base.h"
 #include "fmt/ostream.h"
+#include "nlohmann/json.hpp"
 #include "npeConfig.hpp"
 #include "npeTransferState.hpp"
 
@@ -58,140 +59,92 @@ void npeStats::computeSummaryStats() {
 void npeStats::emitSimStatsToFile(
     const std::string &filepath,
     const std::vector<PETransferState> &transfer_state,
-    const npeDeviceModel& model,
+    const npeDeviceModel &model,
     const npeConfig &cfg) const {
+    nlohmann::json j;
+
+    //---- emit common info ---------------------------------------------------
+    j["common_info"] = {
+        {"device_name", cfg.device_name},
+        {"cycles_per_timestep", cfg.cycles_per_timestep},
+        {"congestion_model_name", cfg.congestion_model_name},
+        {"num_rows", model.getRows()},
+        {"num_cols", model.getCols()},
+        // emit overall stats from the simulation
+        {"dram_bw_util", dram_bw_util},
+        {"link_util", overall_avg_link_util},
+        {"link_demand", overall_avg_link_demand},
+        {"max_link_demand", overall_max_link_demand}};
+
+    //---- emit per transfer data ---------------------------------------------
+    j["noc_transfers"] = nlohmann::json::array();
+    for (const auto &tr : transfer_state) {
+        nlohmann::json transfer;
+        transfer["id"] = tr.params.getID();
+        transfer["src"] = {tr.params.src.row, tr.params.src.col};
+        bool is_multicast = false;
+        if (std::holds_alternative<Coord>(tr.params.dst)) {
+            auto dst = std::get<Coord>(tr.params.dst);
+            transfer["dst"] = {dst.row, dst.col};
+        } else {
+            is_multicast = true;
+            auto [start_coord, end_coord] = std::get<MCastCoordPair>(tr.params.dst);
+            transfer["dst"] = {{start_coord.row, start_coord.col}, {end_coord.row, end_coord.col}};
+        }
+        transfer["total_bytes"] = tr.params.total_bytes;
+        transfer["transfer_type"] = is_multicast ? "MULTICAST" : "UNICAST";
+        transfer["noc_type"] = magic_enum::enum_name(tr.params.noc_type);
+        transfer["injection_rate"] = tr.params.injection_rate;
+        transfer["start_cycle"] = tr.start_cycle;
+        transfer["end_cycle"] = tr.end_cycle;
+
+        transfer["route"] = nlohmann::json::array();
+        for (const auto &link : tr.route) {
+            transfer["route"].push_back(
+                {link.coord.row, link.coord.col, magic_enum::enum_name(nocLinkType(link.type))});
+        }
+
+        j["noc_transfers"].push_back(transfer);
+    }
+
+    //---- emit per timestep data ---------------------------------------------
+    j["timestep_data"] = nlohmann::json::array();
+    for (const auto &ts : per_timestep_stats) {
+        nlohmann::json timestep;
+        timestep["start_cycle"] = ts.start_cycle;
+        timestep["end_cycle"] = ts.end_cycle;
+
+        std::vector<int> active_transfers(ts.live_transfer_ids.begin(), ts.live_transfer_ids.end());
+        std::sort(active_transfers.begin(), active_transfers.end());
+        timestep["active_transfers"] = active_transfers;
+
+        timestep["link_demand"] = nlohmann::json::array();
+        size_t krows = ts.link_demand_grid.getRows();
+        size_t kcols = ts.link_demand_grid.getCols();
+        size_t klinks = ts.link_demand_grid.getItems();
+        for (size_t r = 0; r < krows; r++) {
+            for (size_t c = 0; c < kcols; c++) {
+                for (size_t l = 0; l < klinks; l++) {
+                    float demand = ts.link_demand_grid(r, c, l);
+                    if (demand > 0.001) {
+                        timestep["link_demand"].push_back(
+                            {r, c, magic_enum::enum_name<nocLinkType>(nocLinkType(l)), demand});
+                    }
+                }
+            }
+        }
+        timestep["avg_link_demand"] = ts.avg_link_demand;
+        timestep["avg_link_util"] = ts.avg_link_util;
+
+        j["timestep_data"].push_back(timestep);
+    }
+
     std::ofstream os(filepath);
     if (!os) {
         log_error("Was not able to open stats file '{}'", filepath);
         return;
     }
-
-    fmt::println(os, "{{");
-
-    //---- emit common info ---------------------------------------------------
-    fmt::println(os, "");
-    fmt::println(os, R"("common_info" : {{)");
-    fmt::println(os, R"(  "device_name"           : "{}",)", cfg.device_name);
-    fmt::println(os, R"(  "cycles_per_timestep"   : {},)", cfg.cycles_per_timestep);
-    fmt::println(os, R"(  "congestion_model_name" : "{}",)", cfg.congestion_model_name);
-    fmt::println(os, R"(  "num_rows"              : {},)", model.getRows());
-    fmt::println(os, R"(  "num_cols"              : {})", model.getCols());
-    fmt::println(os, R"(}},)");
-
-    //---- emit per transfer data ---------------------------------------------
-    fmt::println(os, "");
-    fmt::println(os, R"("noc_transfers": [ )");
-    for (const auto &[i, tr] : enumerate(transfer_state)) {
-        // open transfer dict
-        fmt::println(os, "  {{ ");
-        {
-            fmt::println(os, R"(    "id"  : {}, )", tr.params.getID());
-            fmt::println(os, R"(    "src" : [{},{}], )", tr.params.src.row, tr.params.src.col);
-            if (std::holds_alternative<Coord>(tr.params.dst)) {
-                auto dst = std::get<Coord>(tr.params.dst);
-                fmt::println(os, R"(    "dst" : [{},{}], )", dst.row, dst.col);
-            } else {
-                auto [start_coord, end_coord] = std::get<MCastCoordPair>(tr.params.dst);
-                fmt::println(
-                    os,
-                    R"(    "dst" : [[{},{}],[{},{}]], )",
-                    start_coord.row,
-                    start_coord.col,
-                    end_coord.row,
-                    end_coord.col);
-            }
-            fmt::println(os, R"(    "total_bytes"    : {}, )", tr.params.total_bytes);
-            fmt::println(os, R"(    "transfer_type"  : "{}", )", "UNICAST");
-            fmt::println(
-                os, R"(    "noc_type"       : "{}", )", magic_enum::enum_name(tr.params.noc_type));
-            fmt::println(os, R"(    "injection_rate" : {}, )", tr.params.injection_rate);
-            fmt::println(os, R"(    "start_cycle"    : {}, )", tr.start_cycle);
-            fmt::println(os, R"(    "end_cycle"      : {}, )", tr.end_cycle);
-
-            fmt::println(os, R"(    "route" : [ )");
-            for (const auto &[i, link] : enumerate(tr.route)) {
-                fmt::println(
-                    os,
-                    R"(        [{},{},"{}"]{} )",
-                    link.coord.row,
-                    link.coord.col,
-                    magic_enum::enum_name(nocLinkType(link.type)),
-                    (i == tr.route.size() - 1) ? "" : ",");
-            }
-            fmt::println(os, R"(    ])");
-        }
-        // close transfer dict
-        auto comma = (i == transfer_state.size() - 1) ? "" : ",";
-        fmt::println(os, "  }}{} ", comma);
-    }
-    fmt::println(os, "],");
-
-    //---- emit per timestep data ---------------------------------------------
-    fmt::println(os, "");
-    fmt::println(os, R"("timestep_data" : [ )");
-    double overall_avg_link_demand = 0;
-    double overall_max_link_demand = 0;
-    double overall_avg_niu_demand = 0;
-    double overall_max_niu_demand = 0;
-    for (const auto &[i, ts] : enumerate(per_timestep_stats)) {
-        // open timestep dict
-        fmt::println(os, R"(  {{)");
-        {
-            fmt::println(os, R"(    "start_cycle" : {}, )", ts.start_cycle);
-            fmt::println(os, R"(    "end_cycle"   : {}, )", ts.end_cycle);
-
-            fmt::print(os, R"(    "active_transfers" : [)");
-            auto ltids = ts.live_transfer_ids;
-            std::sort(ltids.begin(), ltids.end());
-            for (auto [i, ltid] : enumerate(ltids)) {
-                if (i % 8 == 0)
-                    fmt::print(os, "\n      ");
-                auto comma = (i < ltids.size() - 1) ? "," : " ";
-                fmt::print(os, R"({}{})", ltid, comma);
-            }
-            fmt::println(os, "\n    ],");
-
-            // compute link utilization
-            fmt::println(os, R"(    "link_utilization" : [)");
-            size_t krows = ts.link_demand_grid.getRows();
-            size_t kcols = ts.link_demand_grid.getCols();
-            size_t klinks = ts.link_demand_grid.getItems();
-            double avg_link_demand = 0;
-            bool first = true;
-            for (size_t r = 0; r < krows; r++) {
-                for (size_t c = 0; c < kcols; c++) {
-                    for (size_t l = 0; l < klinks; l++) {
-                        float demand = ts.link_demand_grid(r, c, l);
-                        if (demand > 0.001) {
-                            auto comma = (first) ? "" : ",";
-                            first = false;
-                            avg_link_demand += demand;
-                            TT_ASSERT(demand <= 100.0);
-                            fmt::println(
-                                os,
-                                R"(        {}[{}, {}, "{}", {:.1f}])",
-                                comma,
-                                r,
-                                c,
-                                magic_enum::enum_name<nocLinkType>(nocLinkType(l)),
-                                demand);
-                        }
-                    }
-                }
-            }
-            avg_link_demand /= (krows * kcols * klinks);
-            overall_avg_link_demand += avg_link_demand;
-            overall_max_link_demand = std::max(overall_max_link_demand, avg_link_demand);
-
-            fmt::println(os, "    ]");
-        }
-        // close timestep dict
-        auto comma = (i == per_timestep_stats.size() - 1) ? "" : ",";
-        fmt::println(os, "\n  }}{}", comma);
-    }
-
-    fmt::println(os, "]");
-    fmt::println(os, "}}");
+    os << j.dump(4);
 }
 
 }  // namespace tt_npe
