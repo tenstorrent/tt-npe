ea12ad0b14d24150886018c52085c7118791329f
Brett Grady <bgrady@tenstorrent.com>
2025-02-03 18:17:39 +0000

    update run_noc_events_json_as_workload.sh

--- tt_npe/scripts/run_noc_events_json_as_workload.sh
+++ tt_npe/scripts/run_noc_events_json_as_workload.sh
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
-scripts/convert_noc_events_to_workload.py $1 tmp.yaml #--coalesce_packets
+# get directory of this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+$SCRIPT_DIR/convert_noc_events_to_workload.py $1 tmp.json
 shift
 
-tt_npe_run -w tmp.yaml $@
+tt_npe_run -w tmp.json $@
