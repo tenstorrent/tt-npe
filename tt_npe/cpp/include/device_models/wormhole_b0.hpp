75fa98a3b6c436490d4ffe082f356a7bb9040ccd
Brett Grady <bgrady@tenstorrent.com>
2025-03-25 17:58:05 +0000

    move transfer bw calculation into device model

--- tt_npe/cpp/include/device_models/wormhole_b0.hpp
+++ tt_npe/cpp/include/device_models/wormhole_b0.hpp
@@ -15,11 +15,212 @@ namespace tt_npe {
 class WormholeB0DeviceModel : public npeDeviceModel {
    public:
     WormholeB0DeviceModel() {
-        coord_to_core_type = Grid2D<CoreType>(getRows(), getCols());
+        coord_to_core_type = Grid2D<CoreType>(_num_rows, _num_cols);
         for (const auto &[coord, core_type] : coord_to_core_type_map) {
             coord_to_core_type(coord.row, coord.col) = core_type;
         }
     }
+
+    float interpolateBW(
+        const TransferBandwidthTable &tbt, float max_transfer_bw, size_t packet_size, size_t num_packets) const {
+        TT_ASSERT(packet_size > 0);
+        for (int fst = 0; fst < tbt.size() - 1; fst++) {
+            size_t start_range = tbt[fst].first;
+            size_t end_range = tbt[fst + 1].first;
+            if (packet_size >= start_range && packet_size <= end_range) {
+                float delta = end_range - start_range;
+                float pct = (packet_size - start_range) / delta;
+                float val_delta = tbt[fst + 1].second - tbt[fst].second;
+                float steady_state_bw = (val_delta * pct) + tbt[fst].second;
+
+                float first_transfer_bw = max_transfer_bw;
+                float steady_state_ratio = float(num_packets - 1) / num_packets;
+                float first_transfer_ratio = 1.0 - steady_state_ratio;
+                TT_ASSERT(steady_state_ratio + first_transfer_ratio < 1.0001);
+                TT_ASSERT(steady_state_ratio + first_transfer_ratio > 0.999);
+                float interpolated_bw =
+                    (first_transfer_ratio * first_transfer_bw) + (steady_state_ratio * steady_state_bw);
+
+                return interpolated_bw;
+            }
+        }
+        // if packet is larger than table, assume it has same peak bw as last table entry
+        auto max_table_packet_size = tbt.back().first;
+        if (packet_size >= max_table_packet_size) {
+            return tbt.back().second;
+        }
+
+        TT_ASSERT(false, "interpolation of bandwidth failed");
+        return 0;
+    }
+
+    void updateTransferBandwidth(
+        std::vector<PETransferState> *transfers,
+        const std::vector<PETransferID> &live_transfer_ids) const {
+        const auto &tbt = getTransferBandwidthTable();
+        const float max_transfer_bw = getMaxNoCTransferBandwidth();
+        for (auto &ltid : live_transfer_ids) {
+            auto &lt = (*transfers)[ltid];
+            auto noc_limited_bw = interpolateBW(tbt, max_transfer_bw, lt.params.packet_size, lt.params.num_packets);
+            lt.curr_bandwidth = std::fmin(lt.params.injection_rate, noc_limited_bw);
+        }
+    }
+
+    void modelCongestion(
+        CycleCount start_timestep,
+        CycleCount end_timestep,
+        std::vector<PETransferState> &transfers,
+        const std::vector<PETransferID> &live_transfer_ids,
+        NIUDemandGrid &niu_demand_grid,
+        LinkDemandGrid &link_demand_grid,
+        TimestepStats &sim_stats) const {
+        size_t cycles_per_timestep = end_timestep - start_timestep;
+
+        // assume all links have identical bandwidth
+        float LINK_BANDWIDTH = getLinkBandwidth({{0, 0}, nocLinkType::NOC0_EAST});
+        static auto worker_sink_absorption_rate =
+            getSinkAbsorptionRateByCoreType(CoreType::WORKER);
+
+        // Note: for now doing gradient descent to determine link bandwidth doesn't
+        // appear necessary. Base algorithm devolves to running just a single
+        // iteration (first order congestion only).
+        constexpr int NUM_ITERS = 1;
+        constexpr float grad_fac = 1.0;
+
+        for (int iter = 0; iter < NUM_ITERS; iter++) {
+            // determine effective demand through each link
+            link_demand_grid.reset(0.0f);
+            niu_demand_grid.reset(0.0f);
+            for (auto ltid : live_transfer_ids) {
+                auto &lt = transfers[ltid];
+
+                // account for transfers starting mid-way into timestep, and derate effective
+                // utilization accordingly
+                CycleCount predicted_start = std::max(start_timestep, lt.start_cycle);
+                float effective_demand =
+                    float(end_timestep - predicted_start) / float(cycles_per_timestep);
+                effective_demand *= lt.curr_bandwidth;
+
+                // track demand at src and sink NIU
+                auto src_niu_idx = size_t(
+                    lt.params.noc_type == nocType::NOC0 ? nocNIUType::NOC0_SRC : nocNIUType::NOC1_SRC);
+                niu_demand_grid(lt.params.src.row, lt.params.src.col, src_niu_idx) += effective_demand;
+                auto sink_niu_idx = size_t(
+                    lt.params.noc_type == nocType::NOC0 ? nocNIUType::NOC0_SINK
+                                                        : nocNIUType::NOC1_SINK);
+
+                if (std::holds_alternative<Coord>(lt.params.dst)) {
+                    const auto &dst = std::get<Coord>(lt.params.dst);
+                    niu_demand_grid(dst.row, dst.col, sink_niu_idx) += effective_demand;
+                } else {
+                    const auto &mcast_dst = std::get<MCastCoordPair>(lt.params.dst);
+                    for (auto c : mcast_dst) {
+                        // multicast only loads on WORKER NIUs; other NIUS ignore traffic
+                        if (getCoreType(c) == CoreType::WORKER) {
+                            niu_demand_grid(c.row, c.col, sink_niu_idx) += effective_demand;
+                        }
+                    }
+                }
+
+                for (const auto &link : lt.route) {
+                    auto [r, c] = link.coord;
+                    link_demand_grid(r, c, size_t(link.type)) += effective_demand;
+                }
+            }
+
+            // find highest demand resource on each route to set bandwidth
+            for (auto ltid : live_transfer_ids) {
+                auto &lt = transfers[ltid];
+
+                // find max link demand on route
+                float max_link_demand_on_route = 0;
+                auto update_max_link_demand = [&max_link_demand_on_route](float demand) -> bool {
+                    if (demand > max_link_demand_on_route) {
+                        max_link_demand_on_route = demand;
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+                for (const auto &link : lt.route) {
+                    auto [r, c] = link.coord;
+                    float link_demand = link_demand_grid(r, c, size_t(link.type));
+                    update_max_link_demand(link_demand);
+                }
+                auto min_link_bw_derate = LINK_BANDWIDTH / max_link_demand_on_route;
+
+                // compute bottleneck (min derate factor) for source and sink NIUs
+                auto src_niu_idx = size_t(
+                    lt.params.noc_type == nocType::NOC0 ? nocNIUType::NOC0_SRC : nocNIUType::NOC1_SRC);
+                auto src_bw_demand = niu_demand_grid(lt.params.src.row, lt.params.src.col, src_niu_idx);
+                auto src_bw_derate = lt.params.injection_rate / src_bw_demand;
+
+                auto sink_niu_idx = size_t(
+                    lt.params.noc_type == nocType::NOC0 ? nocNIUType::NOC0_SINK
+                                                        : nocNIUType::NOC1_SINK);
+
+                float sink_bw_derate = 1;
+                if (std::holds_alternative<Coord>(lt.params.dst)) {
+                    const auto &dst = std::get<Coord>(lt.params.dst);
+                    auto sink_bw_demand = niu_demand_grid(dst.row, dst.col, sink_niu_idx);
+                    sink_bw_derate = getSinkAbsorptionRate(dst) / sink_bw_demand;
+                } else {
+                    // multicast transfer speed is set by the slowest sink NIU
+                    const auto &mcast_dst = std::get<MCastCoordPair>(lt.params.dst);
+                    float sink_demand = 0;
+                    for (const auto &loc : mcast_dst) {
+                        if (getCoreType(loc) == CoreType::WORKER) {
+                            sink_demand =
+                                std::min(sink_demand, niu_demand_grid(loc.row, loc.col, sink_niu_idx));
+                        }
+                    }
+                    sink_bw_derate = worker_sink_absorption_rate / sink_demand;
+                }
+
+                auto min_niu_bw_derate = std::min(src_bw_derate, sink_bw_derate);
+
+                if (min_link_bw_derate < 1.0 || min_niu_bw_derate < 1.0) {
+                    float overall_bw_derate = std::min(min_link_bw_derate, min_niu_bw_derate);
+
+                    lt.curr_bandwidth *= 1.0 - (grad_fac * (1.0f - overall_bw_derate));
+                }
+            }
+        }
+
+        // track statistics; see npeStats.hpp for an explanation of how util and demand differ
+        float avg_link_demand = 0;
+        float avg_link_util = 0;
+        float max_link_demand = 0;
+        for (const auto &link_demand : link_demand_grid) {
+            avg_link_demand += link_demand;
+            avg_link_util += std::min(link_demand, LINK_BANDWIDTH);
+            max_link_demand = std::fmax(max_link_demand, link_demand);
+        }
+        avg_link_demand *= 100. / (LINK_BANDWIDTH * link_demand_grid.size());
+        avg_link_util *= 100. / (LINK_BANDWIDTH * link_demand_grid.size());
+        max_link_demand *= 100. / LINK_BANDWIDTH;
+
+        float avg_niu_demand = 0;
+        float max_niu_demand = 0;
+        for (const auto &niu_demand : niu_demand_grid) {
+            avg_niu_demand += niu_demand;
+            max_niu_demand = std::fmax(max_niu_demand, niu_demand);
+        }
+        // Hack: LINK_BANDWIDTH is not always a good approximation of NIU bandwidth
+        avg_niu_demand *= 100. / (LINK_BANDWIDTH * niu_demand_grid.size());
+        max_niu_demand *= 100. / LINK_BANDWIDTH;
+
+        sim_stats.avg_link_demand = avg_link_demand;
+        sim_stats.max_link_demand = max_link_demand;
+        sim_stats.avg_link_util = avg_link_util;
+        sim_stats.avg_niu_demand = avg_niu_demand;
+        sim_stats.max_niu_demand = max_niu_demand;
+
+        // NOTE: copying these is a 10% runtime overhead
+        sim_stats.link_demand_grid = link_demand_grid;
+        sim_stats.niu_demand_grid = niu_demand_grid;
+    }
+
 
     void computeCurrentTransferRate(
         CycleCount start_timestep,
@@ -29,10 +230,26 @@ class WormholeB0DeviceModel : public npeDeviceModel {
         NIUDemandGrid &niu_demand_grid,
         LinkDemandGrid &link_demand_grid,
         TimestepStats &sim_stats,
-        bool enable_congestion_model) const override {}
+        bool enable_congestion_model) const override {
 
-    size_t getCols() const override { return 10; }
-    size_t getRows() const override { return 12; }
+        // Compute bandwidth for this timestep for all live transfers
+        updateTransferBandwidth(&transfer_state, live_transfer_ids);
+
+        // model congestion and derate bandwidth
+        if (enable_congestion_model) {
+            modelCongestion(
+                start_timestep,
+                end_timestep,
+                transfer_state,
+                live_transfer_ids,
+                niu_demand_grid,
+                link_demand_grid,
+                sim_stats);
+        }
+    }
+
+    size_t getCols() const override { return _num_cols; }
+    size_t getRows() const override { return _num_rows; }
 
     float getLinkBandwidth(const nocLinkID &link_id) const override { return 30; }
     float getAggregateDRAMBandwidth() const override { return 256; }
@@ -147,6 +364,9 @@ class WormholeB0DeviceModel : public npeDeviceModel {
     }
 
     protected:
+    const size_t _num_rows = 12;
+    const size_t _num_cols = 10;
+
     TransferBandwidthTable tbt = {
         {0, 0}, {128, 5.5}, {256, 10.1}, {512, 18.0}, {1024, 27.4}, {2048, 30.0}, {8192, 30.0}};
     Grid2D<CoreType> coord_to_core_type;
