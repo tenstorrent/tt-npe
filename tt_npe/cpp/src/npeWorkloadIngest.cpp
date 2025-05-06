4dcf3dd5985d8e221574255f1c299b5c9a428c20
Brett Grady <bgrady@tenstorrent.com>
2025-05-06 14:21:54 +0000

    format cpp/src/npeWorkloadIngest.cpp

--- tt_npe/cpp/src/npeWorkloadIngest.cpp
+++ tt_npe/cpp/src/npeWorkloadIngest.cpp
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
-#include "ingestWorkload.hpp"
-
-#include <filesystem>
 #include <boost/unordered/unordered_flat_set.hpp>
+#include <filesystem>
 
-#include "device_models/wormhole_b0.hpp"
 #include "ScopedTimer.hpp"
+#include "device_models/wormhole_b0.hpp"
+#include "ingestWorkload.hpp"
 #include "npeUtil.hpp"
 #include "npeWorkload.hpp"
 #include "simdjson.h"
@@ -24,7 +23,7 @@ T get_with_default(simdjson::simdjson_result<T> element, T default_value) {
 }
 
 std::optional<npeWorkload> loadJSONWorkloadFormat(const std::string &wl_filename, bool verbose) {
-    ScopedTimer st("",true);
+    ScopedTimer st("", true);
     npeWorkload wl;
 
     if (not std::filesystem::exists(wl_filename)) {
@@ -57,7 +56,8 @@ std::optional<npeWorkload> loadJSONWorkloadFormat(const std::string &wl_filename
                 "JSON workload file '{}' is not structured as an object (map) at the top level!",
                 wl_filename);
             log_error(
-                "Its likely you are trying to load a tt-metal noc trace file; for tt_npe_run use -t to specify a trace file");
+                "Its likely you are trying to load a tt-metal noc trace file; for tt_npe_run use "
+                "-t to specify a trace file");
             return {};
         }
 
@@ -151,7 +151,8 @@ std::optional<npeWorkload> loadJSONWorkloadFormat(const std::string &wl_filename
                     }
 
                     noc_dest = MulticastCoordSet(
-                        Coord{src_device_id, mcast_start_y, mcast_start_x}, Coord{dst_device_id, mcast_end_y, mcast_end_x});
+                        Coord{src_device_id, mcast_start_y, mcast_start_x},
+                        Coord{dst_device_id, mcast_end_y, mcast_end_x});
                 } else {
                     noc_dest = Coord{dst_device_id, dst_y, dst_x};
                 }
@@ -211,8 +212,9 @@ std::optional<npeWorkload> loadJSONWorkloadFormat(const std::string &wl_filename
     return wl;
 }
 
-std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &input_filepath, bool verbose) {
-    ScopedTimer st("",true);
+std::optional<npeWorkload> convertNocTracesToNpeWorkload(
+    const std::string &input_filepath, bool verbose) {
+    ScopedTimer st("", true);
     npeWorkload wl;
 
     const boost::unordered_flat_set<std::string_view> SUPPORTED_NOC_EVENTS = {
@@ -229,8 +231,7 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
         "FABRIC_UNICAST_WRITE",
         "FABRIC_UNICAST_INLINE_WRITE",
         "FABRIC_UNICAST_ATOMIC_INC",
-        "FABRIC_FUSED_UNICAST_ATOMIC_INC"
-    };
+        "FABRIC_FUSED_UNICAST_ATOMIC_INC"};
 
     if (not std::filesystem::exists(input_filepath)) {
         log_error("Provided input file '{}' is not a valid file!", input_filepath);
@@ -242,12 +243,18 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
     try {
         event_data_json = parser.load(input_filepath);
     } catch (const simdjson::simdjson_error &exp) {
-        log_error("Encountered parsing error while reading JSON input file '{}': {}", input_filepath, exp.what());
+        log_error(
+            "Encountered parsing error while reading JSON input file '{}': {}",
+            input_filepath,
+            exp.what());
         return {};
     }
 
     double t0_timestamp = 2e30;
-    boost::unordered_flat_map<std::tuple<std::string_view, int64_t, int64_t>, std::pair<double, double>> per_core_ts;
+    boost::unordered_flat_map<
+        std::tuple<std::string_view, int64_t, int64_t>,
+        std::pair<double, double>>
+        per_core_ts;
 
     for (const auto &event : event_data_json.get_array()) {
         double ts = get_with_default(event["timestamp"].get_int64(), int64_t(0));
@@ -262,7 +269,7 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
             if (it == per_core_ts.end()) {
                 per_core_ts[key] = {ts, ts};
             } else {
-                auto& minmax_ts = it->second;
+                auto &minmax_ts = it->second;
                 minmax_ts.first = std::min(minmax_ts.first, ts);
                 minmax_ts.second = std::max(minmax_ts.second, ts);
             }
@@ -280,11 +287,11 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
     wl.setGoldenResultCycles(max_kernel_cycles);
 
     struct NoCEventSavedState {
-        int64_t sx=0;
-        int64_t sy=0;
-        int64_t dx=0;
-        int64_t dy=0;
-        int64_t num_bytes=0;
+        int64_t sx = 0;
+        int64_t sy = 0;
+        int64_t dx = 0;
+        int64_t dy = 0;
+        int64_t num_bytes = 0;
     };
 
     std::vector<npeWorkloadPhase> phases;
@@ -293,7 +300,8 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
     NoCEventSavedState curr_saved_state_write;
     for (const auto &event : event_data_json.get_array()) {
         std::string_view proc = get_with_default(event["proc"].get_string(), std::string_view{});
-        std::string_view noc_event_type = get_with_default(event["type"].get_string(), std::string_view{});
+        std::string_view noc_event_type =
+            get_with_default(event["type"].get_string(), std::string_view{});
         int64_t num_bytes = get_with_default(event["num_bytes"].get_int64(), int64_t(0));
         int64_t sx = get_with_default(event["sx"].get_int64(), int64_t(-1));
         int64_t sy = get_with_default(event["sy"].get_int64(), int64_t(-1));
@@ -313,7 +321,7 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
             continue;
         }
 
-        if ((noc_event_type == "WRITE_" && num_bytes == 0) || 
+        if ((noc_event_type == "WRITE_" && num_bytes == 0) ||
             (noc_event_type == "READ" && num_bytes == 0)) {
             log_warn("No num_bytes defined for READ/WRITE event; skipping ...");
             continue;
@@ -350,7 +358,7 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
         }
 
         // swap src and dst for read events
-        if (noc_event_type.starts_with("READ")){
+        if (noc_event_type.starts_with("READ")) {
             std::swap(sx, dx);
             std::swap(sy, dy);
         }
@@ -388,26 +396,33 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
 
         NocDestination noc_dest;
         if (noc_event_type == "WRITE_MULTICAST") {
-            int64_t mcast_start_x = get_with_default(event["mcast_start_x"].get_int64(), int64_t(-1));
-            int64_t mcast_start_y = get_with_default(event["mcast_start_y"].get_int64(), int64_t(-1));
+            int64_t mcast_start_x =
+                get_with_default(event["mcast_start_x"].get_int64(), int64_t(-1));
+            int64_t mcast_start_y =
+                get_with_default(event["mcast_start_y"].get_int64(), int64_t(-1));
             int64_t mcast_end_x = get_with_default(event["mcast_end_x"].get_int64(), int64_t(-1));
             int64_t mcast_end_y = get_with_default(event["mcast_end_y"].get_int64(), int64_t(-1));
-            if (mcast_start_x == -1 || mcast_start_y == -1 || mcast_end_x == -1 || mcast_end_y == -1) {
-                log_error("Multicast Transfer event missing 'mcast_start_x/y' or 'mcast_end_x/y'; skipping ... ");
+            if (mcast_start_x == -1 || mcast_start_y == -1 || mcast_end_x == -1 ||
+                mcast_end_y == -1) {
+                log_error(
+                    "Multicast Transfer event missing 'mcast_start_x/y' or 'mcast_end_x/y'; "
+                    "skipping ... ");
                 continue;
             }
             if (noc_type == "NOC_0") {
                 noc_dest = MulticastCoordSet(
-                    Coord{dst_device_id, mcast_start_y, mcast_start_x}, Coord{dst_device_id, mcast_end_y, mcast_end_x});
+                    Coord{dst_device_id, mcast_start_y, mcast_start_x},
+                    Coord{dst_device_id, mcast_end_y, mcast_end_x});
             } else if (noc_type == "NOC_1") {
                 // NOTE: noc_dest coord are reversed for NOC1
                 noc_dest = MulticastCoordSet(
-                    Coord{dst_device_id, mcast_end_y, mcast_end_x}, Coord{dst_device_id, mcast_start_y, mcast_start_x});
+                    Coord{dst_device_id, mcast_end_y, mcast_end_x},
+                    Coord{dst_device_id, mcast_start_y, mcast_start_x});
             }
         } else {
             noc_dest = Coord{dst_device_id, dy, dx};
         }
-        
+
         Coord noc_src_coord{src_device_id, sy, sx};
 
         // if multichip route override exists, add it to the transfer
@@ -416,15 +431,18 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
             npeWorkloadTransferGroupID transfer_group_id = wl.registerTransferGroupID();
             npeWorkloadTransferGroupIndex transfer_group_index = 0;
             for (const auto &route : route_arr) {
-                std::string_view noc_type_str = get_with_default(route["noc"].get_string(), std::string_view{""});
+                std::string_view noc_type_str =
+                    get_with_default(route["noc"].get_string(), std::string_view{""});
                 nocType noc_type = noc_type_str == "NOC_0" ? nocType::NOC0 : nocType::NOC1;
-                DeviceID route_segment_device_id = get_with_default(route["chip_id"].get_int64(), int64_t(-1));
+                DeviceID route_segment_device_id =
+                    get_with_default(route["chip_id"].get_int64(), int64_t(-1));
                 int64_t p1_x = get_with_default(route["p1_x"].get_int64(), int64_t(-1));
                 int64_t p1_y = get_with_default(route["p1_y"].get_int64(), int64_t(-1));
                 int64_t p2_x = get_with_default(route["p2_x"].get_int64(), int64_t(-1));
                 int64_t p2_y = get_with_default(route["p2_y"].get_int64(), int64_t(-1));
 
-                if (route_segment_device_id == -1 || p1_x == -1 || p1_y == -1 || p2_x == -1 || p2_y == -1) {
+                if (route_segment_device_id == -1 || p1_x == -1 || p1_y == -1 || p2_x == -1 ||
+                    p2_y == -1) {
                     log_error("Route override missing required fields; skipping ... ");
                     continue;
                 }
@@ -458,11 +476,12 @@ std::optional<npeWorkload> convertNocTracesToNpeWorkload(const std::string &inpu
     wl.setSourceFilePath(input_filepath);
 
     if (verbose)
-        fmt::println("Workload converted in {:.2f} ms", st.getElapsedTimeMicroSeconds()/1000.0);
+        fmt::println("Workload converted in {:.2f} ms", st.getElapsedTimeMicroSeconds() / 1000.0);
     return wl;
 }
 
-std::optional<npeWorkload> createWorkloadFromJSON(const std::string &wl_filename, bool is_tt_metal_trace_format, bool verbose) {
+std::optional<npeWorkload> createWorkloadFromJSON(
+    const std::string &wl_filename, bool is_tt_metal_trace_format, bool verbose) {
     if (is_tt_metal_trace_format) {
         return convertNocTracesToNpeWorkload(wl_filename, verbose);
     } else {
@@ -470,11 +489,11 @@ std::optional<npeWorkload> createWorkloadFromJSON(const std::string &wl_filename
         if (result.has_value()) {
             return result;
         } else {
-            log_warn("Failed to load workload file; fallback to parsing as tt-metal noc trace ... ");
+            log_warn(
+                "Failed to load workload file; fallback to parsing as tt-metal noc trace ... ");
             return convertNocTracesToNpeWorkload(wl_filename, verbose);
         }
     }
 }
-
 
 }  // namespace tt_npe
