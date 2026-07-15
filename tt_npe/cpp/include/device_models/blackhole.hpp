d71b3e872abf4c06e39c749971bf71c9148156eb
sohaibnadeemTT <sohaibnadeem@tenstorrent.com>
2026-07-15 14:48:24 +0000

    fix wormhole dram BW

--- tt_npe/cpp/include/device_models/blackhole.hpp
+++ tt_npe/cpp/include/device_models/blackhole.hpp
@@ -249,7 +249,8 @@ class BlackholeDeviceModel : public npeDeviceModel {
     }
 
     float getDRAMBandwidthPerController() const override { 
-        return ((core_type_to_inj_rate.at(CoreType::DRAM)+core_type_to_abs_rate.at(CoreType::DRAM)) / 2);
+        constexpr int NUM_CHANNELS_PER_CONTROLLER = 1;
+        return NUM_CHANNELS_PER_CONTROLLER * ((core_type_to_inj_rate.at(CoreType::DRAM)+core_type_to_abs_rate.at(CoreType::DRAM)) / 2);
     }
 
     float getEthBandwidthPerLink() const override { 
